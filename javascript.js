ed0d4434a35d7856ec7580ec8c5d8ccb9ffcb11d
JustinGrunberg <j.e.grunberg@gmail.com>
2024-10-14 22:57:55 +0200

    feat: Add function to play a round of rock, paper, scissor

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
 let answers = ["rock", "paper", "scissor"]
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * 3)
@@ -10,6 +12,33 @@ function getHumanChoice(){
     if(answers.includes(answer)){
         return answer;
     }
-    
 }
+
+function playRound(humanChoice, computerChoice){
+
+    if(humanChoice === computerChoice){
+        console.log("Tie! " + humanChoice + " equals " + computerChoice);
+    }
+    else{
+        switch (humanChoice + " beats " + computerChoice){
+            case "rock beats scissor":
+                humanScore++;
+                console.log("You Win! Rock beats Scissor");
+                break;
+            case "paper beats rock":
+                humanScore++;
+                console.log("You Win! Paper beats Rock");
+                break;
+            case "scissor beats paper":
+                humanScore++;
+                console.log("You Win! Scissor beats Paper");
+                break;
+            default:
+                computerScore++;
+                console.log("You Lose! " + computerChoice + " beats " + humanChoice);
+        }
+    }
+}
+
+
 
